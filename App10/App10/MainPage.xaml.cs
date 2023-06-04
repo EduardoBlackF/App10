97eebd142fbc4f853e9047a6ccd44efcc90ee24d
edubl <edubl@BLACK>
2023-06-04 00:18:16 -0500

    Agregar archivos de proyecto.

---
+++ App10/App10/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn1_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ViewToViewDemo());
+        }
+
+        private async void btn2_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BindingModeDemo());
+        }
+
+        private async void btn3_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListViewDemo());
+        }
+
+        private async void btn4_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PickerDemo());
+        }
+    }
+}
